38bdbd5b7d40689ef03e901132fdcc5c24da5ec7
vidhis <vidhimshah@gmail.com>
2016-09-11 11:37:08 -0700

    Updated method to read data from API and stores into a file which will  be used as a data source later on

--- src/main/java/com/vidhi/cardtest/carddetails/ParseTxnJson.java
+++ src/main/java/com/vidhi/cardtest/carddetails/ParseTxnJson.java
@@ -28,6 +28,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
@@ -348,15 +349,11 @@ public static ArrayList<OutputObj> CalcValNoDonut(Transaction[] arr1){
 
 	public static void loadAllData() throws IOException{
 
-	//	Users/Vidhi/Documents/workspace/carddetails/src/main/java/com/vidhi/cardtest/carddetails/data.txt"
-		//""
-		
 		HttpClient client = new DefaultHttpClient();
 		HttpPost post = new HttpPost("https://prod-api.level-labs.com/api/v2/core/get-all-transactions");
 
-		post.setHeader("Referer", "https://prod-api.level-labs.com/api/v2/core/get-all-transactions");
-		post.setHeader("Accept", "application/json");
-		post.setHeader("Content-type", "application/json");
+		post.addHeader("Accept", "application/json");
+		post.addHeader("Content-type", "application/json");
 		
 		// if you need any parameters
 		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
@@ -367,18 +364,36 @@ public static ArrayList<OutputObj> CalcValNoDonut(Transaction[] arr1){
 		urlParameters.add(new BasicNameValuePair("json-verbose-response", "false"));
 		post.setEntity(new UrlEncodedFormEntity(urlParameters));
 
+		StringEntity input = new StringEntity("{\"args\": {\"uid\": 1110590645, \"token\": \"9B006AA9FC0ACA764A2CDC6E29CF32DF\", \"api-token\": \"AppTokenForInterview\", \"json-strict-mode\": false, \"json-verbose-response\": false}}");
+        input.setContentType("application/json");
+        post.setEntity(input);
+		
+        for (NameValuePair h : urlParameters)
+        {
+            post.addHeader(h.getName(), h.getValue());
+        }
+        
 		HttpResponse response = client.execute(post);
 
+		response.toString();
+		
+		//System.out.println(response);
+		
 		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
 		StringBuffer result = new StringBuffer();
 		String line = "";
 		while ((line = rd.readLine()) != null) {
 		    result.append(line);
+		//    System.out.println(line);
 		}
 		
-		System.out.println(result.toString());
+		//System.out.println(result.toString());
 		
-	}
+		FileWriter fw = new FileWriter("/Users/Vidhi/Documents/workspace/carddetails/src/main/java/com/vidhi/cardtest/carddetails/Transactions.txt");
+		fw.write(result.toString());
+		fw.close();
+		
+    }
 			
 }
